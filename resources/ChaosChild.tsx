8a89bc3e894fd312a39cf796907752be7faf22f0
VictorMendezGjuitar <48413327+VictorMendezGjuitar@users.noreply.github.com>
2019-04-10 04:55:17 +0200

    Recursos
    A partir de ahora todos los sprites mapas y sonidos que subais iran a esta carpeta

---
+++ resources/ChaosChild.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="ChaosChild" tilewidth="32" tileheight="32" tilecount="2016" columns="32">
+ <image source="rpg_maker_vx_rtp_tileset_by_telles0808_d2mslkf.png" width="1024" height="2016"/>
+</tileset>
---
+++ resources/Mapas/2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="ojasd" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="Fuera_casa_inicio.png" width="480" height="480"/>
+</tileset>
