b23e58276f09dcd5f89f6018d73f4d0b82bfa561
Osman Mehmed <48412725+MDMehmed@users.noreply.github.com>
2019-04-15 12:09:50 +0200

    Delete 1.tsx

--- Mapas/1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Ship_Outside_C" tilewidth="32" tileheight="32" tilecount="576" columns="24">
- <image source="Ship_Outside_C.png" width="768" height="768"/>
-</tileset>
