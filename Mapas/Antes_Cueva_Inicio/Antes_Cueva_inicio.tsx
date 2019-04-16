2c1378e10c74e466a1f26105bb9d51f2899e68ba
Osman Mehmed <48412725+MDMehmed@users.noreply.github.com>
2019-04-16 21:30:26 +0200

    Add files via upload

---
+++ Mapas/Antes_Cueva_Inicio/Antes_Cueva_inicio.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Antes_Cueva_inicio" tilewidth="32" tileheight="32" tilecount="570" columns="19">
+ <image source="Antes_Cueva_inicio.png" width="608" height="960"/>
+</tileset>
