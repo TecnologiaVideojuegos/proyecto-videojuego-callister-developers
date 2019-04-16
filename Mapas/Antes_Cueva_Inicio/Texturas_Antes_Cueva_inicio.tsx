adfeb63008a9870fdbd22cbfecfe626c3739ac98
Osman Mehmed <48412725+MDMehmed@users.noreply.github.com>
2019-04-16 21:32:11 +0200

    Delete Texturas_Antes_Cueva_inicio.tsx

--- Mapas/Antes_Cueva_Inicio/Texturas_Antes_Cueva_inicio.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="Antes_Cueva_inicio" tilewidth="32" tileheight="32" tilecount="180" columns="9">
- <image source="Antes_Cueva_inicio.png" width="288" height="640"/>
-</tileset>
