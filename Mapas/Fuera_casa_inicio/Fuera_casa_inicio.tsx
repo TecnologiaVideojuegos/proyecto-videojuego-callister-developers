0ba44610b5d7798100ee169da9f0f42fadd502f9
Osman Mehmed <48412725+MDMehmed@users.noreply.github.com>
2019-04-16 19:07:47 +0200

    Add files via upload

---
+++ Mapas/Fuera_casa_inicio/Fuera_casa_inicio.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Fuera_casa_inicio" tilewidth="32" tileheight="32" tilecount="750" columns="30">
+ <image source="Fuera_casa_inicio.png" width="960" height="800"/>
+</tileset>
