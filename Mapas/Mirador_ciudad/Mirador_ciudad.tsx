c9725d24acea39105952671b3f05e4c85c0a64e4
Osman Mehmed <48412725+MDMehmed@users.noreply.github.com>
2019-04-16 17:36:01 +0200

    Add files via upload

---
+++ Mapas/Mirador_ciudad/Mirador_ciudad.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Mirador_ciudad" tilewidth="32" tileheight="32" tilecount="600" columns="30">
+ <image source="Mirador_ciudad.png" width="960" height="640"/>
+</tileset>
