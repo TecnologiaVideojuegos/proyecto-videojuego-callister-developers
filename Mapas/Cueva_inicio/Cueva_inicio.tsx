7cddccd1539946ddb22a4ab0eb0e5e7725d9f784
Osman Mehmed <48412725+MDMehmed@users.noreply.github.com>
2019-04-16 13:13:56 +0200

    Add files via upload

---
+++ Mapas/Cueva_inicio/Cueva_inicio.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Cueva_inicio" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="Cueva_inicio.png" width="960" height="960"/>
+</tileset>
