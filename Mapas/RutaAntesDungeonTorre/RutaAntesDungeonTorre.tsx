ab14dfc7d3cd2b35816af25cd9f63285b5b44d3c
Osman Mehmed <osman.mehmed@edu.uah.es>
2019-05-08 23:00:55 +0100

    Añadido mapa ruta antes de Dungeon Torre

---
+++ Mapas/RutaAntesDungeonTorre/RutaAntesDungeonTorre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="RutaAntesDungeonTorre" tilewidth="32" tileheight="32" tilecount="6768" columns="144">
+ <image source="RutaAntesDungeonTorre.png" width="4608" height="1504"/>
+</tileset>
