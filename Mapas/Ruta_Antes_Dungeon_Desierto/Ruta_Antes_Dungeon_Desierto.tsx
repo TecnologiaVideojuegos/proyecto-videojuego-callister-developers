a34b94eade1e057b5ccaf7d2c2801938f795bb2f
Osman Mehmed <osman.mehmed@edu.uah.es>
2019-05-12 19:33:16 +0100

    Añadido Mapa: Ruta antes de Dungeon_Desierto

---
+++ Mapas/Ruta_Antes_Dungeon_Desierto/Ruta_Antes_Dungeon_Desierto.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Ruta_Antes_Dungeon_Desierto" tilewidth="32" tileheight="32" tilecount="6768" columns="47">
+ <image source="Ruta_Antes_Dungeon_Desierto.png" width="1504" height="4608"/>
+</tileset>
