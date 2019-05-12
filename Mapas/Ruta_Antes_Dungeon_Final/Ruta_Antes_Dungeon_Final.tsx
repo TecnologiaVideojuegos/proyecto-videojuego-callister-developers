088c7079cdd82f55811df2af5e187ae2d6c0eda9
Osman Mehmed <osman.mehmed@edu.uah.es>
2019-05-12 23:28:50 +0100

    Añadido mapa espectacular sacado de google: https://boards.fireden.net/tg/last/50/50215120/, Ruta_Antes_Dungeon_Final

---
+++ Mapas/Ruta_Antes_Dungeon_Final/Ruta_Antes_Dungeon_Final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="LOOOOOOOOOOOOL" tilewidth="32" tileheight="32" tilecount="875" columns="25">
+ <image source="LOOOOOOOOOOOOL.jpg" width="800" height="1120"/>
+</tileset>
