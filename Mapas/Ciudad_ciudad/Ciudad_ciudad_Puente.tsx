6432041bcbe64dbca54d10ba4e70516ed4aa13c8
Osman Mehmed <osman.mehmed@edu.uah.es>
2019-05-12 13:42:13 +0100

    Añadida transparencia a los puente de la Ciudad y Catedral

---
+++ Mapas/Ciudad_ciudad/Ciudad_ciudad_Puente.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Ciudad_ciudad_Puente" tilewidth="32" tileheight="32" tilecount="3680" columns="40">
+ <image source="Ciudad_ciudad_Puente.png" width="1280" height="2944"/>
+</tileset>
