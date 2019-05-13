8cf58a96df8e3efe2df95450ea7f78e3685420eb
Osman Mehmed <osman.mehmed@edu.uah.es>
2019-05-13 01:10:36 +0100

    Añadido mapa: Ruta_Antes_Cueva_Dungeon_Final

---
+++ Mapas/Ruta_Antes_Cueva_Dugeon_Final/Ruta_Antes_Cueva_Dungeon_Final.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Ruta_Antes_Cueva_Dungeon_Final" tilewidth="32" tileheight="32" tilecount="3337" columns="71">
+ <image source="Ruta_Antes_Cueva_Dungeon_Final.png" width="2272" height="1504"/>
+</tileset>
