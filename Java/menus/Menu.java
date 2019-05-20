eeafeeed4949b4a0233f2fcceb4eecb6dd644cc4
cealsan <cealsan@hotmail.es>
2019-05-20 22:01:57 +0200

    Añadido opcion guardar en Menu
    Mejora menu, añadida opción guardar

--- Java/menus/Menu.java
+++ Java/menus/Menu.java
@@ -47,6 +47,7 @@ public class Menu extends BasicGameState {
         botonEquipo=new Image("resources/Menus/Equipo.png");
         botonFabricacion=new Image("resources/Menus/Fabricacion.png");
         botonSalir=new Image("resources/Menus/Salir.png");
+        botonGuardar=new Image("resources/Menus/Guardar.png");
         punteroInventario=new Image("resources/Menus/PunteroMenu.png");
 
         indicador=0;
@@ -60,7 +61,8 @@ public class Menu extends BasicGameState {
         botonInventario.draw(renderMenu.getX(),renderMenu.getY());
         botonEquipo.draw(renderMenu.getX(),renderMenu.getY()+70);
         botonFabricacion.draw(renderMenu.getX(),renderMenu.getY()+70*2);
-        botonSalir.draw(renderMenu.getX(),renderMenu.getY()+70*3);
+        botonGuardar.draw(renderMenu.getX(),renderMenu.getY()+70*3);
+        botonSalir.draw(renderMenu.getX(),renderMenu.getY()+70*4);
         punteroInventario.draw(p.getX(),p.getY());
         
         
@@ -81,11 +83,11 @@ public class Menu extends BasicGameState {
     public void cEntrada(Input entrada, StateBasedGame game) throws FileNotFoundException, IOException, SlickException, ClassNotFoundException{
         
         if (entrada.isKeyPressed(Input.KEY_S)){
-            if(indicador!=5){
+            if(indicador!=4){
                 p.setY(p.getY()+70);
                 indicador++;
             }else{
-                p.setY(p.getY()-70*5);
+                p.setY(p.getY()-70*4);
                 indicador=0;
             }
               
@@ -95,8 +97,8 @@ public class Menu extends BasicGameState {
                 p.setY(p.getY()-70);
                 indicador--;
             }else{
-                p.setY(p.getY()+70*5);
-                indicador=3;
+                p.setY(p.getY()+70*4);
+                indicador=4;
             }
         }
         
@@ -124,14 +126,7 @@ public class Menu extends BasicGameState {
                     oo.close();
                     guardar.close();
                     break;
-                case 5:
-                    FileInputStream fileInputStream= new FileInputStream("Guardado/Lucia.dat");
-                    ObjectInputStream oi = new ObjectInputStream(fileInputStream);
-                    
-                    Lucia.getLucia().readExternal(oi);
-                    oi.close();
-                    fileInputStream.close();
-                    break;
+
             }
               
         }
