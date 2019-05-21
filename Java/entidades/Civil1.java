b4880a993e2dea7a0b07ec29245970a9ad2aef06
CarlosGolvano <47215458+CarlosGolvano@users.noreply.github.com>
2019-05-21 15:35:07 +0200

    Add files via upload

--- Java/entidades/Civil1.java
+++ Java/entidades/Civil1.java
@@ -15,7 +15,7 @@ import org.newdawn.slick.SlickException;
 public class Civil1 extends NPC{
     
     public Civil1(String nombre, int x, int y) throws SlickException {
-        super("resources/NPC/Civil 1.png", 65, 64, 8, nombre,new int[] {4,4,4,4,4,4,2,2});
+        super("resources/NPC/Civil 1/Civil 1.png", 65, 64, 8, nombre,new int[] {4,4,4,4,4,4,2,2});
         setPosicion(new Punto(x, y));
         genHitboxes(new Punto(super.getPosicion().getX()+22, super.getPosicion().getY()+(47)), 19, 19);
         animaciones(new int[] {4,4,4,4,4,4,2,2}, new int[] {300, 300, 300, 300, 300, 300, 300, 300}, new boolean[]{true, true, true, true,true, true, true, true});
